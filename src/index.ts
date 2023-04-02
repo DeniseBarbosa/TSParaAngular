edfdb82a218087e86afae7b62539c1158587d03b
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-02 08:51:34 -0300

    tipos de variáveis

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+//tipos primitivos: boolean, number, string
+let ligado:boolean = false;
+let nome: string = "Denise";
+
+let idade: number = 20;
+let altura: number = 1.7;
+
+// tipos especiais > null, undefined
+let nulo : null = null;
+let indefinido: undefined = undefined;
+
+//tipos> abragentes: any, void
+let retorno:void
+//aceita qualquer coisa > pode retornar qualquer coisa > um objeto, nulo, texto ou vazio
+let retornoView:any = "s"
+
+
+
